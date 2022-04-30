09aa4e6baf120f45d7781c423766f7ba4220fa3c
DIBYANSHU JHA <85605269+dibyanshujha12345@users.noreply.github.com>
2022-04-30 19:00:55 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,20 @@
+class Atm:
+    card ="XXXXXXXXXXXX4343"
+    cvv=721
+   
+
+    def cashWithdrawl():
+        no=input("Enter card no.")
+        cvv=input("Enter CVV")
+        amt=input("Enter the amount") 
+
+    def moneyDeposit():
+        no1=input("Enter card no.")
+        cvv1=input("Enter cvv")
+        amt1=input("Enter amount")
+
+Dibyanshu=Atm()
+Dibyanshu.cashWithdrawl()
+
+Shashwat=Atm()
+Shashwat.moneyDeposit()
